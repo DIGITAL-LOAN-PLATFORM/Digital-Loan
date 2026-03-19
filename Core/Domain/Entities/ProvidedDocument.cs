0d00c3597d2724d41afb404a22979325bbf01b3c
= <=>
2026-03-19 14:55:10 +0200

    Initial commit

---
+++ Core/Domain/Entities/ProvidedDocument.cs
@@ -0,0 +1,15 @@
+namespace Domain.Entities
+{
+    public class ProvidedDocument
+    {
+        public int Id { get; set; }
+        public int LoanId { get; set; }
+        public string DocumentName { get; set; }
+        public string DocumentFile { get; set;}
+        
+
+       
+
+        
+    }
+}
---
+++ Core/Domain/Entities/Reason.cs
@@ -0,0 +1,9 @@
+namespace Domain.Entities
+{
+    public class Reason
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+       
+    }
+}
