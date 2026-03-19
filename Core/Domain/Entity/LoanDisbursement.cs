f9414f107b506f58d27759af3406105ea11964f0
= <=>
2026-03-19 15:01:29 +0200

    entity

---
+++ Core/Domain/Entity/LoanDisbursement.cs
@@ -0,0 +1,15 @@
+namespace Domain.Entity
+{
+    public class LoanDisbursement
+    {
+        public int Id { get; set; }
+        public LoanApplication LoanApplication { get; set; }
+        public PaymentModality PaymentModality { get; set; }
+        public float DisbursedAmount { get; set; }
+        public string PaymentMode { get; set; }
+        public string principalOffered { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public float InterestRate { get; set; }
+    }
+}
